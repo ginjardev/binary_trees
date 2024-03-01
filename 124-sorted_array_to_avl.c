08816ba2d0152a7bcf9c2e1f98de9cc2796df2ce
Michael Megbenu <msk.megbenu@gmail.com>
2024-03-01 05:02:44 +0200

    124

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -1,42 +1,43 @@
 #include "binary_trees.h"
 
 /**
- * _sorted_array_to_avl - helper func for sorted_array_to_avl
- * @array: input array
- * @start: starting index
- * @end: ending index
- * @parent: pointer to parent node
- * Return: newly created node
- */
-avl_t *_sorted_array_to_avl(int *array, int start, int end, avl_t *parent)
-{
-	avl_t *new;
-	int mid;
-
-	if (start > end)
-		return (NULL);
-	mid = (start + end) / 2;
-	new = calloc(1, sizeof(avl_t));
-	if (!new)
-		return (NULL);
-	new->n = array[mid];
-	new->parent = parent;
-	new->left = _sorted_array_to_avl(array, start, mid - 1, new);
-	new->right = _sorted_array_to_avl(array, mid + 1, end, new);
-	return (new);
-}
-
-
-/**
  * sorted_array_to_avl - builds an AVL tree from an array
- * @array: input array
  * @size: size of array
+ * @array: input array
  * Return: pointer to the root node of the created AVL tree, or NULL on failure
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-
 	if (!array)
 		return (NULL);
-	return (_sorted_array_to_avl(array, 0, size - 1, NULL));
+	return (help_sort(array, 0, size - 1, NULL));
+}
+
+/**
+ * help_sort - helper func for sorted_array_to_avl
+ * @array: input array
+ * @end: final index
+ * @start: beginning index
+ * @parent: pointer to parent node
+ * Return: newly created node
+ */
+avl_t *help_sort(int *array, int start, int end, avl_t *parent)
+{
+	avl_t *new_node;
+	int centre;
+
+	if (start > end)
+		return (NULL);
+	centre = (start + end) / 2;
+	new_node = calloc(1, sizeof(avl_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = array[centre];
+	new_node->parent = parent;
+	new_node->left = help_sort(array, start, centre - 1, new_node);
+	new_node->right = help_sort(array, centre + 1, end, new_node);
+
+	return (new_node);
 }
